aa0d2df2d91793a0bf60726ac3e599a301a88493
Andrew Beiler <andrewbeiler@Andrews-MacBook-Air.local>
2021-09-25 09:27:31 -0500

    Updated spell_check functions to support special chars
    Unit tests for both parse_txt() and check_word() were updated to check logic surrounding special characters.
    
    parse_text() was updated to split using a regular expression looking for any character that is not a number, letter or '. It then removes and
    spaces or blank entries but keep all special characters in the list.
    
    check_word() was updated to check if a entry in the list is a special character before checking the spelling. If it is a special character, than
    entry is skipped.

--- app/spell_check.py
+++ app/spell_check.py
@@ -1,8 +1,13 @@
 from spellchecker import SpellChecker
+import re
 
 
 def parse_txt(raw_input):
-    word_list = raw_input.split()
+    init_word_list = re.split('([^a-zA-Z0-9\'])', raw_input)
+    word_list = []
+    for word in init_word_list:
+        if word != ' ' and word:
+            word_list.append(word)
     return word_list
 
 
@@ -10,6 +15,7 @@ def check_word(word_list):
     check = SpellChecker()
     results = []
     for idx, word in enumerate(word_list):
-        if not check[word]:
-            results.append(idx)
+        if not re.match('([^a-zA-Z0-9])', word):
+            if not check[word]:
+                results.append(idx)
     return results
