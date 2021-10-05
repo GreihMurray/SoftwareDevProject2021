1d2740fd6d3d6f007e67b4e2345a7b135fc5a423
Bryan MacGavin <bryan.macgavin@slu.edu>
2021-10-04 22:03:32 -0500

    Updated the run.py file with code commenting about the file

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+"""
+Main run file for the website.
+After activating the virtual environment in your terminal of choice,
+activate with >>python run.py
+"""
 from app import app
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
