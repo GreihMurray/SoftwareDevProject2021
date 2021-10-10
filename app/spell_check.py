b0bde21270b151f992a37ecaf9ccaa00a062e5be
Andrew Beiler <andrewbeiler@Andrews-Air.attlocal.net>
2021-10-10 04:01:56 -0500

    Updated tokenization of input text
    Updated tokenization of input text to maintain any an all characters(including spaces) rather than only words.
    parse_txt() now also returns a second list wich contains the index values for the words in the input list. This
      will allow for easily determining which content of the input need to spell checked and which simply need to be
      maintained for displaying back to the user.
    Updated check_word to now accept both the input_list and the word_list.
    
    Updated all Unit Tests for this refactor.

--- app/spell_check.py
+++ app/spell_check.py
@@ -2,22 +2,28 @@ from spellchecker import SpellChecker
 import re
 
 def parse_txt(raw_input):
-    init_word_list = re.split('([^a-zA-Z0-9\'])', raw_input)
+    init_word_list = re.split(r'([^a-zA-Z0-9\'])', raw_input)
+    input_list = []
     word_list = []
     for word in init_word_list:
-        if word != ' ' and word:
-            word_list.append(word)
-    return word_list
+        if word == ' ':
+            input_list.append(word)
+        elif re.match(r'([^a-zA-Z0-9\'])',word):
+            input_list.append(word)
+        elif word:
+            word_list.append(len(input_list))
+            input_list.append(word)
+    return input_list, word_list
 
 
-def check_word(word_list):
+def check_word(input_list, word_list):
+    print(word_list)
     check = SpellChecker()
     results = []
-    for idx, word in enumerate(word_list):
-        if not re.match('([^a-zA-Z0-9])', word):
-            if not check[word]:
-                results.append(idx)
-
+    for word_idx in word_list:
+        if not re.match('([^a-zA-Z0-9])', input_list[word_idx]):
+            if not check[input_list[word_idx]]:
+                results.append(word_idx)
     return results
 
 def word_candidates(word_to_check):
