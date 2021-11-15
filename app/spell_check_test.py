1dda1093d60c4df810413cda8359868ff9370421
Greih Murray <murrayg0216@outlook.com>
2021-11-15 14:42:42 -0600

    Slight Changes to tests
    One of the tests broke during rebase, has now been fixed

--- app/spell_check_test.py
+++ app/spell_check_test.py
@@ -50,7 +50,7 @@ class TestSpellCheckMethods(unittest.TestCase):
         self.assertEqual(check_other_lang(input_list, word_list, lang_dictionaries['Irish']), [0, 4, 6, 8])
         # Some words in the below test do not appear in the dict due to lack of instances. If the dictionary is updated, this may change
         input_list, word_list = parse_txt('Briathra Amós , aoire de chuid Theacóá.')
-        self.assertEqual(check_other_lang(input_list, word_list, lang_dictionaries['Irish']), [2, 12])
+        self.assertEqual(check_other_lang(input_list, word_list, lang_dictionaries['Irish']), [2, 6, 12])
         input_list, word_list = parse_txt('     ')
         self.assertEqual(check_other_lang(input_list, word_list, lang_dictionaries['Irish']), [])
 
