518d4e21f7ecec803ad080d40665e7d1243b1262
Andrew Beiler <andrewbeiler@Andrews-Air.attlocal.net>
2021-12-04 22:30:43 -0600

    logicCntrl() Refactor and Simplfied check_word()

--- app/spell_check_test.py
+++ app/spell_check_test.py
@@ -24,12 +24,17 @@ class TestSpellCheckMethods(unittest.TestCase):
                           [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 24, 28]))
 
     def test_check_word(self):
-        self.assertEqual(check_word(["Here", ' ', 'is', ' ', 'a', ' ', 'mispelled', ' ', 'word'], [0, 2, 4, 6, 8]), [6])
-        self.assertEqual(check_word(['letz', ' ', 'yuse', ' ', 'compleatly', ' ', 'mispelled', ' ', 'centense'],
-                                    [0, 2, 4, 6, 8]), [0, 2, 4, 6, 8])
-        self.assertEqual(check_word(['Let\'z', ' ', 'test', ' ', 'this', '!', ' ', 'It', ' ', 'is', ' ', 'grait', ' ',
-                                     'to', ' ', 'be', ' ', '@', ' ', 'the', ' ', 'balgame', ',', ' ', 'with', ' ', 'my',
-                                    ' ', 'friend','!'], [0, 2, 4, 7, 9, 11, 13, 15, 19, 21, 24, 26, 28]), [0, 11, 21])
+        self.assertTrue(check_word("here"))
+        self.assertTrue(check_word("is"))
+        self.assertTrue(check_word("a"))
+        self.assertFalse(check_word("mispelled"))
+        self.assertTrue(check_word("word"))
+        self.assertFalse(check_word("letz"))
+        self.assertFalse(check_word("yuse"))
+        self.assertFalse(check_word("@"))
+        self.assertFalse(check_word("!"))
+        self.assertFalse(check_word("let\'z"))
+        self.assertTrue(check_word("let\'s"))
 
     def test_sort_by_count(self):
         self.assertEqual(sort_by_count([['test', 10], ['text', 12], ['other', 8], ['thing', 4]]), [['text', 12], ['test', 10], ['other', 8], ['thing', 4]])
